49fe3b79e6da1f0a41919dcb8bdcb840429eff0d
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-05 14:20:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,184 @@
+<html>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<head>
+  <link rel="stylesheet" href="css/style.css"
+</head>
+<body>
+<div id="content">
+  <svg height="900" width="780">
+      <path id="MyPath"
+            d="M550,50 Q550,100 300,290 Q150,350 200,650 L130,780" />
+    <path id="MyPath2"
+            d="M550,50 Q550,100 600,300 Q650,650 250,700 L200,800 150,770 80,900" />
+      <path id="MyPath3"
+            d="M550,90 Q450,400 160,780" />
+        <path id="MyPath4"
+            d="M420,230 L710,460" />
+        <path id="which"
+            d="M480,340 Q500,370 610,340" />
+        <path id="trembles"
+            d="M460,550 Q400,440 490,380  " />
+        <path id="and"
+            d="M470,520 Q520,500 680,100" />    
+        <path id="joins"
+            d="M550,360 Q560,500 440,600" />
+            <path id="world"
+            d="M270,600 Q200,440 450,260" />
+            <path id="imagination"
+            d="M300,560 Q300,510 470,280" />
+            <path id="and2"
+            d="M370,580 400,520" />
+            <path id="ghost"
+            d="M300,620 Q450,680 650,500" />
+
+    <text class="t1" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#MyPath"  method="stretch" spacing="auto">
+        The silence is broken: into the nature
+    My soul sails out, 
+      </textPath>
+    </text>
+
+    <text class="t2" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#MyPath2"  method="stretch" spacing="auto">
+        Carrying the song of life on his brow,
+     To meet the flowers and birds.
+      </textPath>
+    </text>
+
+    <text class="t3" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#MyPath3"  method="stretch" spacing="auto">
+        When my heart returns in the solitude, 
+     She is very sad,
+  Looking back on the dead passions
+    Lying on Love's ruin. 
+      </textPath>
+    </text>
+
+      <text x="510" y="250" style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2em;
+  filter: drop-shadow(2px 2px 2px #a60);">
+        I AM
+    </text>
+          <text x="500" y="280" style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2em;
+  filter: drop-shadow(2px 2px 2px #a60);">
+        LIKE
+    </text>
+          <text x="475" y="310" style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2em;
+  filter: drop-shadow(2px 2px 2px #a60);">
+        A LEAF
+    </text>
+
+       <text x="470" y="-150" style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 0.8em;
+  filter: drop-shadow(2px 2px 2px #a60); transform:rotate(40deg);">
+        Hanging
+    </text>
+    <text x="475" y="-130" style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 1.4em;
+  filter: drop-shadow(2px 2px 2px #a60); transform:rotate(40deg);">
+        over
+    </text>
+
+      <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 1.4em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#MyPath4"  method="stretch" spacing="auto">
+        HOPE
+      </textPath>
+    </text>
+
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#which"  method="stretch" spacing="auto">
+        which
+      </textPath>
+    </text>
+
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2.3em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#trembles"  method="stretch" spacing="auto">
+        TREMBLES
+      </textPath>
+    </text>
+
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 4em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#and"  method="stretch" spacing="auto">
+        AND
+      </textPath>
+    </text>
+
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size:4em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#joins"  method="stretch" spacing="auto">
+        JOINS
+      </textPath>
+    </text>
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size:3.7em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#world"  method="stretch" spacing="auto">
+        THE WORLD's
+      </textPath>
+    </text>
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size:3.5em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#imagination"  method="stretch" spacing="auto">
+        imagination
+      </textPath>
+    </text>
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size:2em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#and2"  method="stretch" spacing="auto">
+        AND
+      </textPath>
+    </text>
+    <text style="fill: #57d962;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size:4em;
+  filter: drop-shadow(2px 2px 2px #a60);" textLength="500" lengthAdjust="spacingAndGlyphs">
+      <textPath xlink:href="#ghost"  method="stretch" spacing="auto">
+        JOINS
+      </textPath>
+    </text>
+  </svg>
+</div>
+</body>
+</body>
+</html>
